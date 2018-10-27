7aa5322aed7f136c4f2e6e58ed04c2581c084172
Azraf Chowdhury <achowdhur@ltu.edu>
2018-10-27 08:35:00 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
-  array.each_with_index do |index, name|
+  array.each.with_index(1) do |index, name|
     puts "#{index}. #{name}"
   end
   return array
