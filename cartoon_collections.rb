57b4e8d819e3798866d892fe36872478355d28a5
Azraf Chowdhury <achowdhur@ltu.edu>
2018-10-27 08:40:58 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ end
 
 def summon_captain_planet(array) # code an argument here
   # Your code here
-  array.map! {
+  array.map! { |name| name.capitalize }
 end
 
 def long_planeteer_calls# code an argument here
