b578b85daa2176729200b83c84236480b88acc2e
Azraf Chowdhury <achowdhur@ltu.edu>
2018-10-27 08:45:23 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ end
 
 def summon_captain_planet(array) # code an argument here
   # Your code here
-  array.map! { |name| name.capitalize }
+  array.map! { |name| name.capitalize + "!" }
   array.each { |name| puts name + "!"}
 end
 
