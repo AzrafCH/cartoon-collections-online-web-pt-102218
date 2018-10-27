c9d8368fbc3ba8523b62b1974ef27d2d90c1f02f
Azraf Chowdhury <achowdhur@ltu.edu>
2018-10-27 08:34:23 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,9 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
   array.each_with_index do |index, name|
-    puts "#{index+1}. #{name}"
+    puts "#{index}. #{name}"
   end
-  array
+  return array
 end
 
 def summon_captain_planet# code an argument here
